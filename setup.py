805b8b4ef9ec493d42521ec9252474ef4535247e
Sylvain Viollon <sviollon@minddistrict.com>
2016-06-29 15:52:07 +0200

    Move code in an src directory and actually include something in the resulting package.

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ version = '0.1a2.dev0'
 setup(
     name='koppeltaal_schema',
     version=version,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
